1d3837d8044f0d8c2a5dd3a71e9b5ff045516e45
RebeccaJ21 <rebecca.prows@gmail.com>
2017-11-13 23:46:43 -0800

    Question4

--- app.js
+++ app.js
@@ -79,13 +79,21 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(testArray){ //eslint-disable-line
-
+function sumArray(testArray) { //eslint-disable-line
+var sumOfArray=0;
+for (var i=0; i<testArray.length; i++) {
+  console.log(testArray[i]);
+  sumOfArray= sum(testArray[i], sumOfArray)[0];
+}
+var fourthString= testArray.join(',');
+fourthString+= ' was passed in as an array of numbers, and ' + sumOfArray + ' is their sum.';
+console.log(fourthString);
+return [sumOfArray, fourthString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
